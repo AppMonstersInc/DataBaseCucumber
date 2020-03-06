3cc2fdbae6a99f9768dd4d9d79e8dd7b531ced84
elenagrash <12345>
2020-03-06 16:11:05 -0600

    create student scenario and selenium date format method

---
+++ src/test/java/step_definitions/students_steps/click.java
@@ -0,0 +1,48 @@
+package step_definitions.students_steps;
+
+import cucumber.api.java.en.When;
+import org.openqa.selenium.support.ui.Select;
+import utilities.SeleniumUtils;
+
+public class click {
+//    //  @When("I fill out all information about new student with value")
+//    public void i_fill_out_all_information_about_new_student_with_value(io.cucumber.datatable.DataTable dataTable) {
+//        data = dataTable.asMap(String.class, String.class);
+//        addStudentPage.firstNameInputBox.click();
+//        SeleniumUtils.pause(1);
+//        addStudentPage.firstNameInputBox.sendKeys(data.get("FIRST_NAME"));
+//        addStudentPage.lastNameInputBox.click();
+//        addStudentPage.lastNameInputBox.sendKeys(data.get("LAST_NAME"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.emailInputBox.sendKeys(data.get("EMAIL_ADDRESS"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.passwordInputBox.sendKeys(data.get("PASSWORD"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.confirmPasswordInputBox.sendKeys(data.get("PASSWORD"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.joiningDateInputBox.click();
+//        addStudentPage.joiningDateInputBox.sendKeys(data.get("JOIN_DATE"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.subjectInputBox.sendKeys(subject);
+//        addStudentPage.mobileNumberInputBox.sendKeys(data.get("PHONE"));
+//        Select select = new Select(addStudentPage.genderSelectBox);
+//        select.selectByIndex(1);
+//        addStudentPage.admissionNoInputBox.clear();
+//        addStudentPage.admissionNoInputBox.sendKeys("125");
+//        SeleniumUtils.pause(1);
+//        addStudentPage.birthDayInputBox.sendKeys(data.get("BIRTH_DATE"));
+//        SeleniumUtils.pause(1);
+//        addStudentPage.majorInputBox.sendKeys(data.get("MAJOR"));
+//        Select select2 = new Select(addStudentPage.batchSelectBox);
+//        select2.selectByIndex(2);
+//        addStudentPage.sectionInputBox.clear();
+//        addStudentPage.sectionInputBox.sendKeys(data.get("SECTION"));
+//        addStudentPage.premanentAddressInputBox.sendKeys(data.get("PREMANENT_ADDRESS"));
+//        addStudentPage.companyNameInputBox.sendKeys(faker.company().name());
+//        addStudentPage.startDayInputBox.sendKeys("11/22/2020");
+//        addStudentPage.streetInputBox.sendKeys("123 N. California, Apt 1");
+//        addStudentPage.stateInputBox.sendKeys(faker.address().state());
+//        addStudentPage.titleInputBox.sendKeys(faker.team().name());
+//        addStudentPage.cityInputBox.sendKeys(faker.address().city());
+//        addStudentPage.zipCodeInputBox.sendKeys(faker.address().zipCode().replace("-", ""));
+}
